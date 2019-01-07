c2e3451a116f484b1ff0885b87a31d26f514f973
yshjft <jericoh@naver.com>
2019-01-07 23:42:46 +0900

    1.8까지 완료 1.9 진행 중

---
+++ practice/index.js
@@ -0,0 +1,19 @@
+/*
+// alert("I'm Working.");
+// console.log("I'm Working");
+// const var let
+let a=221;
+let b=a-5;
+a=4
+const what="hys";
+//이모티콘도 가능함
+console.log(b, a);
+*/
+
+/*
+//true 1, false 0
+const wat =true;
+*/
+
+//cammel case
+const daysOfWeek
